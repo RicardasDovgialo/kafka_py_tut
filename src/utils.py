2962a536ed732636687eeee68b1830da0e05686a
Ricardas Dovgialo <ricardasmykolasd@gmail.com>
2024-02-08 09:27:04 +0200

    docker + producer

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,7 @@
 from google.cloud import secretmanager
 
+
+
 def get_secret(secret_id: str, version_id="latest"):
     """
     Get information about the given secret. This only returns metadata about
@@ -10,3 +12,22 @@ def get_secret(secret_id: str, version_id="latest"):
     response = client.access_secret_version(name=name)
 
     return response.payload.data.decode('UTF-8')
+
+KAFKA_CONFIG = {
+    "bootstrap.servers": "...:9020",
+    "security.protocol": "sasl_ssl",
+    "sasl.mechanisim": "PLAIN",
+    "sasl.username": "...",
+    "sasl.password": get_secret("local_kafka_pass"),
+}
+
+KAFKA_SCHEMA_REGISTRY = {
+    "url": "...",
+    "basic.auth.user.info": get_secret("local_sr_api_creds"),
+}
+
+x = KAFKA_SCHEMA_REGISTRY.pop("url")
+
+print(x)
+print("haha")
+print(KAFKA_SCHEMA_REGISTRY)
--- src/youtube_watcher.py
+++ src/youtube_watcher.py
@@ -5,6 +5,11 @@ import requests
 import utils as u
 import json
 
+from confluent_kafka.schema_registry import SchemaRegistryClient
+from confluent_kafka.serialization import StringSerializer
+from confluent_kafka.schema_registry.avro import AvroSerializer
+from confluent_kafka import SerializingProducer
+
 def get_videos(page_token=None):
     params = {
         "key": u.get_secret("youtube_API_key"),
@@ -36,15 +41,34 @@ def video_details(video_id, page_token=None):
         "views": int(data["statistics"].get("viewCount", 0)),
     }
 
-def push_to_kafka():
+def on_delivery(error, record):
     pass
+
+def push_to_kafka(data):
+
+    schema_registry_client = SchemaRegistryClient(u.KAFKA_SCHEMA_REGISTRY)
+    yt_vid_value_schema = schema_registry_client.get_latest_version("youtube_videos-value")
+
+    conf = u.KAFKA_CONFIG | {
+        "key.serializer": StringSerializer(),
+        "value.serializer": AvroSerializer(schema_registry_client, yt_vid_value_schema.schema.schema_str)
+    }
+    producer = SerializingProducer(conf)
+
+    video_id = data.pop("id")
+    producer.produce(
+        topic="youtube_videos",
+        key=video_id,
+        value={k.upper():v for k,v in data.items()},
+        on_delivery=on_delivery
+    )
 
 def main():
     videos = [x["contentDetails"]["videoId"] for x in get_videos()]
 
     for video_id in videos:
         video_data = video_details(video_id)
-        push_to_kafka
+        push_to_kafka(video_data)
         
 
     
